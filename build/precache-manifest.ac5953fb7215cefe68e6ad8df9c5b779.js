1c5427a5f39be26f4115393a3eea9817c222b5f8
farid555 <mohammad.uzzaman2@centria.fi>
2020-10-19 10:42:58 +0300

    uibuild

---
+++ build/precache-manifest.ac5953fb7215cefe68e6ad8df9c5b779.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb9fa995a2e26503e7348a66ce76af77",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b88085bdf11bce492333",
+    "url": "/static/css/main.e54d449c.chunk.css"
+  },
+  {
+    "revision": "db444e3213bd27236985",
+    "url": "/static/js/2.e14de027.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e14de027.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b88085bdf11bce492333",
+    "url": "/static/js/main.eed1864c.chunk.js"
+  },
+  {
+    "revision": "f4d2ac87a76b6331fd91",
+    "url": "/static/js/runtime-main.4234e0d5.js"
+  }
+]);
