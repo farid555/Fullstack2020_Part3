b29cb495a37e4a18ed3a129e3bf02db0141c2763
farid555 <mohammad.uzzaman2@centria.fi>
2020-11-13 11:50:39 +0200

    not yet done 3.21

--- index.js
+++ index.js
@@ -1,118 +1,119 @@
-require("dotenv").config();
-const express = require("express");
-const app = express();
-const morgan = require("morgan");
-const cors = require("cors");
-const Person = require("./models/persons");
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const morgan = require('morgan')
+const cors = require('cors')
+const Person = require('./models/persons')
 
-app.use(express.json());
-app.use(express.static("build"));
-app.use(cors());
+app.use(express.json())
+app.use(express.static('build'))
+app.use(cors())
 
-morgan.token("data", (req) => {
-  if (req.method === "POST") {
-    return JSON.stringify(req.body);
+
+morgan.token('data', (req) => {
+  if (req.method === 'POST') {
+    return JSON.stringify(req.body)
   } else {
-    return null;
+    return null
   }
-});
+})
 app.use(
-  morgan(":method :url :status :res[content-length] - :response-time ms :data")
-);
+  morgan(':method :url :status :res[content-length] - :response-time ms :data')
+)
 
-app.get("/info", (req, res) => {
+app.get('/info', (req, res) => {
   Person.find({}).then((people) => {
-    const info = `There is information of ${people.length} people in the phonebook.`;
-    const date = Date();
-    res.send(`<p>${info}</p><p>${date}</p>`);
-  });
-});
+    const info = `There is information of ${people.length} people in the phonebook.`
+    const date = Date()
+    res.send(`<p>${info}</p><p>${date}</p>`)
+  })
+})
 
-app.get("/api/persons", (req, res) => {
+app.get('/api/persons', (req, res) => {
   Person.find({}).then((persons) => {
-    res.json(persons.map((person) => person.toJSON()));
-  });
-});
+    res.json(persons.map((person) => person.toJSON()))
+  })
+})
 
-app.get("/api/persons/:id", (req, res, next) => {
+app.get('/api/persons/:id', (req, res, next) => {
   Person.findById(req.params.id)
     .then((person) => {
       if (person) {
-        res.json(person);
+        res.json(person)
       } else {
-        res.status(404).end();
+        res.status(404).end()
       }
     })
-    .catch((error) => next(error));
-});
+    .catch((error) => next(error))
+})
 
-app.delete("/api/persons/:id", (req, res, next) => {
+app.delete('/api/persons/:id', (req, res, next) => {
   Person.findByIdAndRemove(req.params.id)
-    .then((result) => {
-      res.status(204).end();
+    .then(() => {
+      res.status(204).end()
     })
 
-    .catch((error) => next(error));
-});
-app.put("/api/persons/:id", (req, res, next) => {
-  const body = req.body;
+    .catch((error) => next(error))
+})
+app.put('/api/persons/:id', (req, res, next) => {
+  const body = req.body
 
   const person = {
     name: body.name,
     number: body.number,
-  };
+  }
 
   Person.findByIdAndUpdate(req.params.id, person, { new: true })
     .then((updatedPerson) => {
-      res.json(updatedPerson);
+      res.json(updatedPerson)
     })
-    .catch((error) => next(error));
-});
+    .catch((error) => next(error))
+})
 
-app.post("/api/persons", (req, res, next) => {
-  const body = req.body;
+app.post('/api/persons', (req, res, next) => {
+  const body = req.body
 
   if (body.name === undefined) {
     return res.status(400).json({
-      error: "name is missing!",
-    });
+      error: 'name is missing!',
+    })
   }
   if (body.number === undefined) {
     return res.status(400).json({
-      error: "number is missing!",
-    });
+      error: 'number is missing!',
+    })
   }
 
   const person = new Person({
     id: Math.floor(Math.random() * 25),
     name: body.name,
     number: body.number,
-  });
+  })
   person
     .save()
     .then((savedPerson) => {
-      res.json(savedPerson.toJSON);
+      res.json(savedPerson.toJSON)
     })
-    .catch((error) => next(error));
-});
+    .catch((error) => next(error))
+})
 const errorHandler = (error, req, res, next) => {
-  console.error(error.message);
-  if (error.name === "CastError") {
-    return res.status(400).send({ error: "malformed id" });
-  } else if (error.name === "ValidationError") {
-    return res.status(400).json({ error: error.message });
+  console.error(error.message)
+  if (error.name === 'CastError') {
+    return res.status(400).send({ error: 'malformed id' })
+  } else if (error.name === 'ValidationError') {
+    return res.status(400).json({ error: error.message })
   }
-  next(error);
-};
-app.use(errorHandler);
+  next(error)
+}
+app.use(errorHandler)
 
 const unknownEndpoint = (req, res) => {
-  res.status(404).send({ error: "unknown endpoint" });
-};
+  res.status(404).send({ error: 'unknown endpoint' })
+}
 
-app.use(unknownEndpoint);
+app.use(unknownEndpoint)
 
-const PORT = PORT;
+const PORT = process.env.PORT
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+  console.log(`Server running on port ${PORT}`)
+})
--- mongo.js
+++ mongo.js
@@ -1,32 +1,46 @@
 const mongoose = require('mongoose')
 
 if (process.argv.length < 3) {
-    console.log('Please provide the password as an argument: node mongo.js <password>')
-    process.exit(1)
+  console.log('Please provide the password as an argument: node mongo.js <password>')
+  process.exit(1)
 }
 
 const password = process.argv[2]
+const name = process.argv[3]
+const number = process.argv[4]
 
 const url =
-    `mongodb+srv://fullstack:${password}@cluster0-ostce.mongodb.net/persons?retryWrites=true`
+  `mongodb+srv://fullstack:${password}@cluster0-ostce.mongodb.net/persons?retryWrites=true`
 //`mongodb+srv://fullstack:<PASSWORD>@cluster0-ostce.mongodb.net/note-app?retryWrites=true`
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
 
-const noteSchema = new mongoose.Schema({
-    content: String,
-    date: Date,
-    important: Boolean,
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String
+
 })
 
-const Note = mongoose.model('Note', noteSchema)
+//const Note = mongoose.model('Note', personSchema)
 
-const note = new Note({
-    content: 'HTML is Easy',
-    date: new Date(),
-    important: true,
-})
 
-note.save().then(result => {
-    console.log('note saved!')
+const Person = mongoose.model('Person', personSchema)
+
+if (name === undefined || number === undefined) {
+  Person
+    .find({})
+    .then(persons => {
+      console.log('Phonebook: ')
+      persons.map(person => console.log(person.name, person.number))
+      mongoose.connection.close()
+    })
+} else {
+  const person = new Person({
+    name: name,
+    number: number
+  })
+
+  person.save().then(() => {
+    console.log(`the name ${name} and number ${number} was added to the phonebook`)
     mongoose.connection.close()
-})
+  })
+}
